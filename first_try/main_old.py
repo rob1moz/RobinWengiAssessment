0131f0377ad8e5070f4632efc5a0772f29a83be3
robin <robin.wengi@outlook.com>
2024-06-12 23:37:43 +0200

    Initial loading of the project

---
+++ first_try/main_old.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+# If you want predictable result than set the seed to 0. To generate new random numbers then comment the line below
+np.random.seed(0)
+
+number_of_simulation = 1000
+
+cost_of_capital = 0.08
+
+## Modeling claim frequency
+lam = 1
+claim_frequency = np.random.poisson(lam,number_of_simulation)
+## Modeling claim severity
+alpha = 1.2
+theta = 2
+claim_severity = (np.random.pareto(alpha, np.sum(claim_frequency)) + 1) * theta
+
+
+## Determine the losses with XL contract "USD 10m xs USD 5m"
+limit = 10
+deductible = 5
+
+# Apply the limit and deductible to individual claims
+claim_severity_with_xs = np.maximum(np.minimum(limit,claim_severity-deductible),0)
+
+# Determine the total loss per simulated period
+xs_loss = np.zeros_like(claim_frequency, dtype=float)
+xs_loss[claim_frequency > 0] = [claim_severity_with_xs[np.cumsum(claim_frequency)[i] - claim_frequency[i]:np.cumsum(claim_frequency)[i]].sum() for i in range(len(claim_frequency)) if claim_frequency[i] > 0]
+
+
+xs_mean_loss = xs_loss.mean()
+xs_VaR_99 = np.quantile(xs_loss,0.99)
+
+xs_TVar_99 = xs_loss[xs_loss>xs_VaR_99].mean()
+
+xs_premium = xs_mean_loss + (xs_TVar_99-xs_mean_loss) * cost_of_capital
+
+## Determine the losses under the Xl with AAD contract "10m xs 5m, AAD 2m"
+aad = 2
+aad_xs_loss = np.maximum(xs_loss - aad,0)
+
+aad_xs_mean_loss = np.average(aad_xs_loss)
+aad_xs_VaR_99 = np.quantile(aad_xs_loss,0.99)
+## Determine the losses under the Xl with AAL contract "10m xs 5m, AAL 12m"
+aal = 12
+aal_xs_loss = np.minimum(xs_loss,aal)
+
+## Determine the losses under the Xl with AAL contract "10m xs 5m, AAD 2m, AAL 12m"
+
+aal_aad_xs_loss = np.minimum(np.maximum(xs_loss - aad,0),aal)
+
+
+## Patterns
+
+y1 = 0.3
+y2 = 0.6
+y3 = 0.1
+
+loss1 = claim_severity*y1
+loss2 = claim_severity*y2
+loss3 = claim_severity*y3
+
+xs_loss1 = np.maximum(np.minimum(limit,loss1-deductible),0)
+xs_loss2 = np.maximum(np.minimum(limit,loss1+loss2-deductible)-xs_loss1,0)
+xs_loss3 = np.maximum(np.minimum(limit,loss1+loss2+loss3-deductible)-xs_loss1-xs_loss2,0)
+
+xs_y1 = xs_loss1.sum()/xs_loss.sum()
+xs_y2 = xs_loss2.sum()/xs_loss.sum()
+xs_y3 = xs_loss3.sum()/xs_loss.sum()
---
+++ main.py
@@ -0,0 +1,186 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+class MonteCarlo():
+    def __init__(self, inputs):
+
+        np.random.seed(inputs['random_seed_value'])
+
+        self.frequency_vector = self.get_random_vector(inputs['frequency_distribution'],
+                                                       inputs['frequency_parameter'],
+                                                       inputs['number_of_simulation'])
+
+        self.severity_vector = self.get_random_vector(inputs['severity_distribution'],
+                                                      inputs['severity_parameter'],
+                                                      np.sum(self.frequency_vector))
+
+    def get_random_vector(self, a_distribution, a_parameter_set, a_number_of_simulation):
+        if a_distribution == 'poisson':
+            random_vector = np.random.poisson(a_parameter_set['lambda'], a_number_of_simulation)
+        elif a_distribution == 'pareto':
+            random_vector = (np.random.pareto(a_parameter_set['alpha'], a_number_of_simulation) + 1) * \
+                            a_parameter_set['theta']
+        else:
+            print('The selected distribution in not yet implemented')
+        return random_vector
+    
+    def plot_frequency(self):
+        return sns.displot(self.frequency_vector, bins=5, kde=True,kde_kws={"bw_adjust": 5})
+    
+    def plot_severity(self):
+        return sns.displot(self.severity_vector, bins=100, kde=True,kde_kws={"bw_adjust": 0.5},log_scale=True)
+
+
+class Xl_contract():
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.monte_carlo = MonteCarlo(self.inputs)
+
+        # 1. loss vector for USD 10m xs USD 5m
+        self.xs_loss = self.get_xs_loss_vector()
+        # 2. loss vector for 10m xs 5m, AAD 2m
+        self.aad_loss = self.get_loss_vector_with_aad_and_aal(self.xs_loss, self.inputs['AAD'], np.inf)
+        # 3. loss vector for 10m xs 5m, AAL 12m
+        self.aal_loss = self.get_loss_vector_with_aad_and_aal(self.xs_loss,0, self.inputs['AAL'])
+        # 4. loss vector for 10m xs 5m, AAD 2m, AAL 12m
+        self.aad_aal_loss = self.get_loss_vector_with_aad_and_aal(self.xs_loss,self.inputs['AAD'],self.inputs['AAL'])
+
+
+        # Create a DataFrame with all relevant result for each contract from 1. to 4.
+        self.summary = self.get_result_summary()
+
+    def get_xs_loss_vector(self):
+        primary_insurer_pattern = np.ones((1,len(self.inputs['claim_pattern']))) * self.inputs['claim_pattern']
+
+        cumulative_individual_claim = self.monte_carlo.severity_vector[..., np.newaxis] * np.cumsum(primary_insurer_pattern)
+
+        # Apply the limit and deductible to individual claims and get a vector of severity under xs conditions
+        individual_claims_with_xs = np.maximum(np.minimum(
+            self.inputs['limit'], cumulative_individual_claim - self.inputs['deductible']), 0)
+        individual_claims_with_xs[..., 1:] = np.diff(individual_claims_with_xs)
+
+        # Allocate the individual_claims_with_xs to the simulated periods according to the frequency of each period
+        xs_loss = np.zeros((self.inputs['number_of_simulation'], individual_claims_with_xs.shape[1]))
+        for i in range(individual_claims_with_xs.shape[1]):
+            xs_loss[..., i] = self.allocate_claim_to_simulation_period(individual_claims_with_xs[..., i])
+        return xs_loss
+
+    def get_loss_vector_with_aad_and_aal(self,a_loss_vector, aad, aal):
+        aad_aal_loss = np.minimum(np.maximum(np.cumsum(a_loss_vector, axis=1) - aad, 0), aal)
+        aad_aal_loss[..., 1:] = np.diff(aad_aal_loss)
+        return aad_aal_loss
+
+    def allocate_claim_to_simulation_period(self, an_individual_claim_vector):
+
+        claim_frequency = self.monte_carlo.frequency_vector
+        relevant_interval = np.cumsum(claim_frequency)
+
+        claim_vector = np.zeros_like(claim_frequency, dtype=float)
+
+        claim_vector[claim_frequency > 0] = [
+            an_individual_claim_vector[
+            relevant_interval[i] - claim_frequency[i]:relevant_interval[i]].sum()
+            for i in range(len(claim_frequency)) if claim_frequency[i] > 0]
+
+        return claim_vector
+
+    def VaR(self, a_loss_vector, a_level):
+        return np.quantile(a_loss_vector, a_level)
+
+    def TVaR(self, a_loss_vector, a_level):
+        return a_loss_vector[a_loss_vector >= self.VaR(a_loss_vector, a_level)].mean()
+
+    def premium(self,a_loss_vector,a_level):
+        mean = a_loss_vector.mean()
+        tvar = self.TVaR(a_loss_vector,a_level)
+        cost_of_capital = self.inputs['cost_of_capital']
+        return mean + (tvar-mean) * cost_of_capital
+
+    def reinsurer_pattern(self,a_loss_array):
+        pattern_list = list(a_loss_array.sum(axis=0)/a_loss_array.sum())
+        pattern_list_formatted = [ '%.3f' % elem for elem in pattern_list ]
+        return str(pattern_list_formatted)
+
+    def get_contract_results(self,a_loss_array):
+        a_loss_vector = a_loss_array.sum(axis=1)
+        contract_result_summary = {
+            'Average Loss': a_loss_vector.mean(),
+            'VaR_99': self.VaR(a_loss_vector,0.99),
+            'TVaR_99': self.TVaR(a_loss_vector,0.99),
+            'VaR_995': self.VaR(a_loss_vector,0.995),
+            'Premium': self.premium(a_loss_vector, 0.99),
+            'Pattern': self.reinsurer_pattern(a_loss_array)
+        }
+        return contract_result_summary
+
+    def get_result_summary(self):
+
+        summary ={
+            '10m xs 5m': self.get_contract_results(self.xs_loss),
+            '10m xs 5m, AAD 2m': self.get_contract_results(self.aad_loss),
+            '10m xs 5m, AAL 12m': self.get_contract_results(self.aal_loss),
+            '10m xs 5m, AAD 2m, AAL 12m': self.get_contract_results(self.aad_aal_loss)
+        }
+
+        return pd.DataFrame(summary)
+
+    def the_basic_challenge(self):
+        return self.get_result_summary().loc[['Average Loss','VaR_99'],'10m xs 5m']
+
+    def introducing_AAD_and_AAL(self):
+        return self.get_result_summary().loc[['Average Loss','VaR_99']]
+
+    def applying_a_simple_pricing_model(self):
+        return self.get_result_summary().loc[['Premium']]
+
+    def patterns(self):
+        return self.get_result_summary().loc[['Pattern']].T
+
+
+
+class Portfolio(Xl_contract):
+    def __init__(self, input_list):
+        self.input_list = input_list
+
+        # Define a dictionary with all the line of business objects
+        self.lob = self.set_all_lob()
+
+
+    def set_all_lob(self):
+        lob ={}
+        for i in self.input_list:
+            lob[i['LOB']] = Xl_contract(i)
+        return lob
+
+
+    def get_portfolio_underwriting_risk(self, a_loss_kind = 'xs_loss'):
+        premiums = []
+        losses = []
+        for v in self.lob.values():
+            a_lob_loss = getattr(v, a_loss_kind).sum(axis=1)
+            premiums.append(v.premium( a_lob_loss, 0.99))
+            losses.append(a_lob_loss)
+        return sum(losses) - sum(premiums)
+
+
+    def get_portfolio_results(self,a_loss_kind):
+
+        a_loss_vector = self.get_portfolio_underwriting_risk(a_loss_kind)
+        contract_result_summary = {
+            'TVaR_99': self.TVaR(a_loss_vector,0.99),
+            'VaR_995': self.VaR(a_loss_vector,0.995)
+        }
+        return contract_result_summary
+
+    def get_portfolio_summary(self):
+
+        summary ={
+            '10m xs 5m': self.get_portfolio_results('xs_loss'),
+            '10m xs 5m, AAD 2m': self.get_portfolio_results('aad_loss'),
+            '10m xs 5m, AAL 12m': self.get_portfolio_results('aal_loss'),
+            '10m xs 5m, AAD 2m, AAL 12m': self.get_portfolio_results('aad_aal_loss')
+        }
+
+        return pd.DataFrame(summary)
